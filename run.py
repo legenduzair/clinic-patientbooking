fc94045c17b05f49313e38f10c94db14df96b80c
legenduzair <uzairoomer@live.com>
2021-12-26 15:53:00 +0000

    Add main menu function and print statements to introduce patient booking system

--- run.py
+++ run.py
@@ -14,3 +14,27 @@ SHEET = GSPREAD_CLIENT.open('clinic-patientbooking')
 
 PATIENT = SHEET.worksheet('patient_registration')
 APPOINTMENT = SHEET.worksheet('appointment_registration')
+
+def main_menu():
+    """
+    Main menu where the user can select between six diverse options
+    """
+    print("""
+            --------------Main Menu--------------
+                1. Register New Patient
+                2. Book a Test
+                3. Update Patient Details
+                4. Search for Patient Details
+                5. Delete Patient Details
+                6. Exit\n
+            """)
+    
+def main():
+    main_menu()
+
+
+print("--------------------Welcome to LabClinic--------------------")
+print("----------A client registration system that allows----------")
+print("---------------you to register patient details---------------")
+print("-----------------and book laboratory tests-----------------\n")
+main()
